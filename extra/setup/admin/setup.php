1bee45ee57108279b56dbf635f0792bbcee53c4e
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2015-11-06 17:45:30 +0100

    Style formats handling

--- extra/setup/admin/setup.php
+++ extra/setup/admin/setup.php
@@ -8,35 +8,41 @@
  *
  *
  *********************/
-function _blank_tinymce($init) {
-	$style_formats = json_decode($init['style_formats']);
-	if(empty($style_formats) || !is_array($style_formats)) {
+function _blank_tinymce( $init ) {
+	if ( array_key_exists( 'style_formats', $init ) ) {
+		$style_formats = json_decode( $init['style_formats'] );
+	} else {
 		$style_formats = array();
 	}
-	$style_formats = array_merge($style_formats, array(
-	   array(
-			'title'   => 'Lien bouton',
-			'selector'   => 'a',
-			'classes' => 'link-button'
-		), array(
-			'title'   => 'Lien important',
-			'selector'   => 'a',
-			'classes' => 'link-important'
-		), array(
+	$style_formats         = array_merge( $style_formats, array(
+		array(
+			'title'    => 'Lien bouton',
+			'selector' => 'a',
+			'classes'  => 'link-button'
+		),
+		array(
+			'title'    => 'Lien important',
+			'selector' => 'a',
+			'classes'  => 'link-important'
+		),
+		array(
 			'title'   => 'Chapô',
 			'block'   => 'div',
 			'classes' => 'chapo',
 			'wrapper' => true
-		), array(
-			'title'   => 'Titre avec bordure',
-			'selector'   => 'h1, h2, h3, h4',
-			'classes' => 'border-title'
+		),
+		array(
+			'title'    => 'Titre avec bordure',
+			'selector' => 'h1, h2, h3, h4',
+			'classes'  => 'border-title'
 		)
-	));
-	$init['style_formats'] = json_encode($style_formats);
+	) );
+	$init['style_formats'] = json_encode( $style_formats );
+
 	return $init;
 }
-add_filter('tiny_mce_before_init', '_blank_tinymce');
+
+add_filter( 'tiny_mce_before_init', '_blank_tinymce' );
 
 /**********************
  *
