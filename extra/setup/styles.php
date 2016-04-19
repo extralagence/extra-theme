266026e12aa06411e574476e18e02ec3ea5035e4
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2016-04-19 14:37:59 +0200

    Add mobile menu css by default

--- extra/setup/styles.php
+++ extra/setup/styles.php
@@ -15,5 +15,7 @@ function extra_theme_enqueue_styles() {
 	wp_enqueue_style( 'extra-header', THEME_URI . '/assets/css/header.less', array(), false, 'all' );
 	// EXTRA LAYOUT
 	wp_enqueue_style( 'extra-footer', THEME_URI . '/assets/css/footer.less', array(), false, 'all' );
+	// EXTRA LAYOUT
+	wp_enqueue_style( 'extra-mobile-menu', THEME_URI . '/assets/css/mobile-menu.less', array(), false, 'all' );
 }
 add_action('wp_enqueue_scripts', 'extra_theme_enqueue_styles', 5);
