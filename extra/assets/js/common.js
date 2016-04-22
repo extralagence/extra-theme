7892bcdd5db9f86d8cb9e342d43acecd6761f50a
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2016-04-22 17:36:57 +0200

    Mobile Menu JS

--- extra/assets/js/common.js
+++ extra/assets/js/common.js
@@ -6,11 +6,17 @@
 //
 ///////////////////////////////////////
 $(document).ready(function(){
+
 	// AJAX SVG SPRITE
 	$.get(extra_common_params.theme_url + "/extra/assets/img/sprite.svg", function (data) {
 		var div = document.createElement("div");
 		div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
 		document.body.insertBefore(div, document.body.childNodes[0]);
 		$("html").addClass("extra-svg-loaded");
+	});
+
+	// MENU MOBILE
+	$("#mobile-menu-switch-manager").on("change", function() {
+		$("html").toggleClass("mobile-menu-open", $(this).is(":checked"));
 	});
 });
