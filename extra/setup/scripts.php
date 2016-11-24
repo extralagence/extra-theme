f8a2024b7e3e4fd90ce721f86a020ef7fa47ad05
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2016-11-24 10:00:02 +0100

    :sparkles: Added Theme version constant

--- extra/setup/scripts.php
+++ extra/setup/scripts.php
@@ -8,9 +8,9 @@
 ///////////////////////////////////////
 function extra_theme_enqueue_scripts() {
 	// FLUIDIVS
-	wp_enqueue_script( 'fluidvids', THEME_URI . '/assets/js/fluidvids.min.js', array( 'jquery', 'extra' ), EXTRA_VERSION, true );
+	wp_enqueue_script( 'fluidvids', THEME_URI . '/assets/js/fluidvids.min.js', array( 'jquery', 'extra' ), EXTRA_THEME_VERSION, true );
 	// COMMON
-	wp_enqueue_script('extra-common', THEME_URI.'/assets/js/common.js', array('jquery', 'extra'), EXTRA_VERSION, true);
+	wp_enqueue_script('extra-common', THEME_URI.'/assets/js/common.js', array('jquery', 'extra'), EXTRA_THEME_VERSION, true);
 	wp_localize_script( 'extra-common', 'extra_common_params', array(
 		'theme_url' => get_stylesheet_directory_uri()
 	));
--- extra/setup/styles.php
+++ extra/setup/styles.php
@@ -8,14 +8,14 @@
 ///////////////////////////////////////
 function extra_theme_enqueue_styles() {
 	// EXTRA CONTENT
-	wp_enqueue_style( 'extra-content', THEME_URI . '/assets/css/content.less', array(), EXTRA_VERSION, 'all' );
+	wp_enqueue_style( 'extra-content', THEME_URI . '/assets/css/content.less', array(), EXTRA_THEME_VERSION, 'all' );
 	// EXTRA LAYOUT
-	wp_enqueue_style( 'extra-layout', THEME_URI . '/assets/css/layout.less', array(), EXTRA_VERSION, 'all' );
+	wp_enqueue_style( 'extra-layout', THEME_URI . '/assets/css/layout.less', array(), EXTRA_THEME_VERSION, 'all' );
 	// EXTRA LAYOUT
-	wp_enqueue_style( 'extra-header', THEME_URI . '/assets/css/header.less', array(), EXTRA_VERSION, 'all' );
+	wp_enqueue_style( 'extra-header', THEME_URI . '/assets/css/header.less', array(), EXTRA_THEME_VERSION, 'all' );
 	// EXTRA LAYOUT
-	wp_enqueue_style( 'extra-footer', THEME_URI . '/assets/css/footer.less', array(), EXTRA_VERSION, 'all' );
+	wp_enqueue_style( 'extra-footer', THEME_URI . '/assets/css/footer.less', array(), EXTRA_THEME_VERSION, 'all' );
 	// EXTRA LAYOUT
-	wp_enqueue_style( 'extra-mobile-menu', THEME_URI . '/assets/css/mobile-menu.less', array(), EXTRA_VERSION, 'all' );
+	wp_enqueue_style( 'extra-mobile-menu', THEME_URI . '/assets/css/mobile-menu.less', array(), EXTRA_THEME_VERSION, 'all' );
 }
 add_action('wp_enqueue_scripts', 'extra_theme_enqueue_styles', 5);
